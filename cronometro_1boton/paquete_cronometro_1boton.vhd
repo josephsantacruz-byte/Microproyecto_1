e412f2eb3485dd051e36d5128d3fc6eff8fc9219
josephsantacruz-byte <josephsantacruz@unicauca.edu.co>
2026-09-22 11:24:19 -0500

    Create paquete_cronometro_1boton.vhd

---
+++ cronometro_1boton/paquete_cronometro_1boton.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package cronometro_pkg is
+
+    -- Divisor de reloj
+    component divisor_reloj is
+        Port (
+            clk_50 : in  STD_LOGIC;
+            reset  : in  STD_LOGIC;
+            clk_1s : out STD_LOGIC
+        );
+    end component;
+
+    -- Cronómetro de 1 Botón (con clk_1s actualizado)
+    component timer_1boton is
+        Port (
+            clk_1s   : in  STD_LOGIC;
+            btn      : in  STD_LOGIC;
+            minutos  : out STD_LOGIC_VECTOR (3 downto 0);
+            seg_dec  : out STD_LOGIC_VECTOR (3 downto 0);
+            seg_uni  : out STD_LOGIC_VECTOR (3 downto 0)
+        );
+    end component;
+
+    -- Decodificador de 7 segmentos
+    component hex_decoder is
+        Port (
+            bin_in   : in  STD_LOGIC_VECTOR (6 downto 0);
+            seg_dec  : out STD_LOGIC_VECTOR (6 downto 0);
+            seg_uni  : out STD_LOGIC_VECTOR (6 downto 0)
+        );
+    end component;
+
+end package cronometro_pkg;
